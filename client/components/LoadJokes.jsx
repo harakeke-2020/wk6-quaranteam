fb979efcc5efa233a5a03d1582809e4d89df75a5
Hamish Stobo <51815749+HHSNZ@users.noreply.github.com>
2020-02-14 11:49:34 +1300

    api file sending form data

--- client/components/LoadJokes.jsx
+++ client/components/LoadJokes.jsx
@@ -11,6 +11,7 @@ class LoadJokes extends React.Component {
   }
 
   handleClick = (e) => {
+    console.log(e.target.value)
     e.preventDefault()
     this.setState({
       [e.target.name]: e.target.value
@@ -28,22 +29,18 @@ class LoadJokes extends React.Component {
       <div>
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="type">What type of joke would you like?</label>
-          <select id="type" name="type">
+          <select defaultValue={this.state.type} id="type" name="type" onChange={this.handleClick} >
             <option value="" disabled selected hidden>Select one</option>
-            <option onClick={this.handleClick} value="general">General</option>
-            <option onClick={this.handleClick} value="programming">Ten</option>
-            <option onClick={this.handleClick} value="knock-knock">Knock-knock</option>
+            <option value="general">General</option>
+            <option value="programming">Ten</option>
+            <option value="knock-knock">Knock-knock</option>
           </select>
-          {/* <button type="submit">Confirm!</button>
-        </form>
-        <form onSubmit={this.handleSubmit}> */}
           <label htmlFor="quantity">How many jokes ya want?</label>
-          <select id="quantity" name="quantity">
-            <option onClick={this.handleClick} value="" disabled selected hidden>Select one</option>
-            <option onClick={this.handleClick} value="one">One</option>
-            <option onClick={this.handleClick} value="ten">Ten</option>
+          <select defaultValue={this.state.quantity} id="quantity" name="quantity" onChange={this.handleClick} >
+            <option value="" disabled selected hidden>Select one</option>
+            <option value="one">One</option>
+            <option value="ten">Ten</option>
           </select>
-          {/* <input type="text" onChange={(e) => console.log(e.target.value)} /> */}
           <button type="submit" >Confirm!</button>
         </form>
       </div>
