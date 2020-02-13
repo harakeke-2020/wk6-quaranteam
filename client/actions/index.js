937816d762dac383beab075676808c88cba98008
Jared James <57459274+Jared-James@users.noreply.github.com>
2020-02-13 17:32:04 +1300

    start work on waiting functionality

---
+++ client/actions/index.js
@@ -0,0 +1,44 @@
+import request from 'superagent'
+
+export const SHOW_ERROR = 'SHOW_ERROR'
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+export const REQUEST_POSTS = 'REQUEST_POSTS'
+
+export const requestPosts = () => {
+  return {
+    type: REQUEST_POSTS
+  }
+}
+
+export const receivePosts = (posts) => {
+  return {
+    type: RECEIVE_POSTS,
+    posts: posts.map(post => post.data)
+  }
+}
+
+export const showError = (errorMessage) => {
+  return {
+    type: SHOW_ERROR,
+    errorMessage: errorMessage
+  }
+}
+
+export function fetchPosts (subreddit) {
+  // Action creator that returns a function instead of an action.
+  return (dispatch) => {
+    // Perform requestPosts method. Returns {type: REQUEST_POSTS}
+    dispatch(requestPosts())
+
+    // Make a request to the server using superagent
+    return request
+      .get(`/api/v1/reddit/subreddit/${subreddit}`)
+      .then(res => {
+        console.log(res.body)
+        dispatch(receivePosts(res.body))
+      })
+      .catch(err => {
+        dispatch(showError(err.message))
+      })
+  }
+}
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux'
+import waiting from './waiting'
 
 export default combineReducers({
-
+  waiting
 })
