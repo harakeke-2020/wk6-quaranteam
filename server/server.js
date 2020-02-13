b84927bd4c8d3570168fcc6594db0ac803130595
0qy <linqianye333@gmail.com>
2020-02-14 10:56:23 +1300

    routes not currently working

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const path = require('path')
 const express = require('express')
-const routes = require('./routes')
 
-const jokesRoutes = require('./jokes')
+const jokes = require('./routes/jokes')
+const routes = require('./routes/routes')
 const server = express()
 
 server.use(express.static(path.join(__dirname, '../public')))
 
-server.use('api/v1', jokesRoutes)
+server.use('api/v1', routes)
 
 module.exports = server
