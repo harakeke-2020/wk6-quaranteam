d3ad51918ae15783e9e2e8a431208e775fb0ae61
Hamish Stobo <51815749+HHSNZ@users.noreply.github.com>
2020-02-14 15:29:19 +1300

    commit before exit

--- client/components/JokesList.jsx
+++ client/components/JokesList.jsx
@@ -4,28 +4,25 @@ import { connect } from 'react-redux'
 import Joke from './Joke'
 
 // component contains subreddits props which comes from global state
-class JokesList extends React.Component {
-  render () {
-    console.log(this.props)
-    return (
-      <div>
-        {
-        }
-        <h2>joke</h2>
-        {/* {props.jokes.map((joke, i) =>
-          <Joke
-            key={i}
-            joke={joke}
-          />
-        )} */}
-      </div>
-    )
-  }
+const JokesList = (props) => {
+  console.log(props)
+
+  return (
+    <div>
+      <h2>joke</h2>
+      {/* {props.map((joke, i) =>
+        <Joke
+          key={i}
+          joke={joke}
+        />
+      )} */}
+    </div>
+  )
 }
 
 function mapStateToProps (state) {
   return {
-    jokes: state.jokes
+    jokes: state
   }
 }
 
